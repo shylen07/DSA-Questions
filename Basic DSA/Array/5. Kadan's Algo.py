fc699956f5261fe07131cfcc3ca853bc04b70019
Devender Singh <dkathait269@gmail.com>
2023-08-18 20:40:12 +0530

    Kadane's Algo

---
+++ Basic DSA/Array/5. Kadan's Algo.py
@@ -0,0 +1,17 @@
+#max subarray sum
+def kadane(arr,n):
+    current_sum = 0
+    max_sum = 0
+    for i in range(n):
+        current_sum +=arr[i]
+        if current_sum > max_sum:
+            max_sum = current_sum
+        if current_sum < 0:
+            current_sum =0
+    return max_sum
+arr=[-1,2,-2,5,7,-3,1]
+n =len(arr)
+ans = kadane(arr,n)
+print(ans)
+
+    
